233634083bd741174ca8d3623d73c06cc3096bff
y_sahara <classa1990@gmail.com>
2024-11-09 20:42:08 +0900

    framer motionでのアニメーションの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 
 function App() {
   return (
-    <main className="h-screen bg-slate-600">
+    <main className="h-screen bg-slate-800">
       <div className="container mx-auto">
         {/* header */}
-        <motion.header 
-        initial={{y:-100}}
-        animate={{y:0}}
-        transition={{
-          type:"spring",
-          duration:1,
-          delay:0.5,
-          stiffness:130,
-
-        }}
-        className="flex justify-between items-center text-white h-16">
+        <motion.header
+          initial={{ y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            duration: 1,
+            delay: 0.5,
+            stiffness: 130,
+          }}
+          className="flex justify-between items-center text-white h-16"
+        >
           <span className="font-bold text-2xl">framer website</span>
           <nav>
             <ul className="flex items-center gap-4">
@@ -33,7 +33,58 @@ function App() {
         </motion.header>
 
         {/* hero */}
-        <section></section>
+        <section className="lg:py-48">
+          <div className="flex items-center justify-between">
+            {/* left */}
+            <div className="space-y-3">
+              <motion.h2
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{
+                  type: "spring",
+                  duration: 1,
+                  delay: 0.5,
+                  stiffness: 130,
+                }}
+                className="text-white lg:text-9xl md:text-8xl font-bold lg:max-w-[40rem]"
+              >
+                framer 3D Website
+              </motion.h2>
+              <motion.p 
+              initial={{ y: 100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                type: "spring",
+                duration: 1,
+                delay: 0.8,
+                stiffness: 130,
+              }}
+              className="text-white lg:max-w-[40rem] lg:text-5xl md:text-4xl font-semibold">
+                The<span className="text-orange-500"> next</span> generation
+              </motion.p>
+              <motion.div 
+               initial={{ y: 100, opacity: 0 }}
+               animate={{ y: 0, opacity: 1 }}
+               transition={{
+                 type: "spring",
+                 duration: 1,
+                 delay: 0.9,
+                 stiffness: 130,
+               }}
+              className="flex items-center gap-4">
+                <button className="py-3 px-6 rounded-md border-2 my-4 hover:translate-y-1 duration-150">
+                  <span className="font-bold text-white">More Details</span>
+                </button>
+                <button className="py-3 px-6 rounded-md border-2 border-orange-500 my-4 hover:translate-y-1 duration-150">
+                  <span className="font-bold text-orange-500">Samples</span>
+                </button>
+              </motion.div>
+            </div>
+
+            {/* 3D model */}
+            <div></div>
+          </div>
+        </section>
       </div>
     </main>
   );
